44b04d9b5a8201fe12979419a173b83ac534f6a4
Katangalac <katangalachristian@yahoo.com>
2026-01-13 04:05:10 -0500

    correction du socket

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,7 +22,7 @@ import morgan from "morgan";
 const app = express();
 const PORT = process.env.PORT || 3000;
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: "https://projectmanager-wb93.onrender.com",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
