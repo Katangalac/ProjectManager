f1ad90888efaf67d96a40fcd16922a0ebea3ee84
Katangalac <katangalachristian@yahoo.com>
2026-01-24 10:00:44 -0500

    correction de la connexion socket

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -25,7 +25,7 @@ const corsOption = {
   origin: process.env.CLIENT_URL||"http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
 };
 
 app.use(express.json());
