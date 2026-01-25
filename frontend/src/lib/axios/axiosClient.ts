52745e300f2cec0364453ca9d7b3ef51c263fcac
Katangalac <katangalachristian@yahoo.com>
2026-01-25 02:22:17 -0500

    Modification du mécanisme de session

--- frontend/src/lib/axios/axiosClient.ts
+++ frontend/src/lib/axios/axiosClient.ts
@@ -11,11 +11,12 @@ export const axiosClient = axios.create({
 
 // Log pour debug (retirer en prod)
 axiosClient.interceptors.request.use((config) => {
+    console.log("AXIOS-INTERCEPT-1")
     const token = getUserStore().getState().token;
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
-
+    console.log("AXIOS-INTERCEPT-2")
   console.log('🔹 Request:', config.method?.toUpperCase(), config.url);
   console.log('🔹 Request-Auth:', token);
   console.log('🍪 With credentials:', config.withCredentials);
