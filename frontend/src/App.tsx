6a048efa5ce54aaf85cedb8749e00b3ff0695c58
Katangalac <katangalachristian@yahoo.com>
2026-01-27 01:57:34 -0500

    Correction du mécanisme de connexion avec google

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RootRedirect from "./pages/RootRedirect.tsx";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import AuthCallback from "@/pages/AuthCallback";
+import AuthCallback from "./pages/AuthCallback";
 import DashBoard from "./pages/DashBoard.tsx";
 import ProfilePage from "./pages/ProfilePage.tsx";
 import ProtectedRoute from "./components/commons/ProtectedRoute.tsx";
