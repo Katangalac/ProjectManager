5a7e7df8cfcc75278a5673f4646e13932dfdb5b9
Katangalac <katangalachristian@yahoo.com>
2026-02-18 03:33:18 -0500

    Correction de l'url de connexion au serveur websocket

--- frontend/src/lib/socket/socketClient.ts
+++ frontend/src/lib/socket/socketClient.ts
@@ -3,15 +3,18 @@ import { io, Socket } from "socket.io-client";
 /**
  * Client socket-io pour faire du temps réel pour les messages, et autre
  */
-export const socket: Socket = io(import.meta.env.VITE_API_URL || "http://localhost:3000", {
-  withCredentials: true,
-  autoConnect: false,
-  auth: (cb) => {
-    cb({
-      token: JSON.parse(sessionStorage.getItem("user-storage") ?? "null")?.state.token,
-    });
-  },
-});
+export const socket: Socket = io(
+  import.meta.env.VITE_SOCKET_SERVER_URL || "http://localhost:3000",
+  {
+    withCredentials: true,
+    autoConnect: false,
+    auth: (cb) => {
+      cb({
+        token: JSON.parse(sessionStorage.getItem("user-storage") ?? "null")?.state.token,
+      });
+    },
+  }
+);
 
 socket.on("connect", () => console.log("Connected to server"));
 socket.on("disconnect", (reason) => console.log("Disconnected:", reason));
